9c3465b4cef887b0617034ab306acc082c9151ad
Renna42 <saki@izumino.cat>
2025-10-27 01:57:03 +0800

    garbage

--- src/utils/findFriend.ts
+++
@@ -1,7 +0,0 @@
-import { FRIENDS } from "@config";
-
-const findFriend = (friendName: string) => {
-  return FRIENDS.filter(friend => friend.name === friendName)[0];
-};
-
-export default findFriend;
