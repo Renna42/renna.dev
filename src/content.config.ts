685e80ca9bfac4a5128d23bc2e14e017729a392e
Renna Z. <is@renna.dev>
2026-03-24 06:14:34 +0800

    add giscus

--- src/content.config.ts
+++ src/content.config.ts
@@ -15,6 +15,7 @@ const blog = defineCollection({
       tags: z.array(z.string()).default(["others"]),
       description: z.string(),
       canonicalURL: z.string().optional(),
+      comment: z.boolean().default(true),
     }),
 });
 
